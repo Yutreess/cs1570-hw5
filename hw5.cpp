a10e37a09f68c53a3ff2c3d43c7ba92ecd0693fb
Justus <justus_pete@hotmail.com>
2018-09-24 21:00:14 -0500

    Added main cpp file

---
+++ hw5.cpp
@@ -0,0 +1,17 @@
+// Programmer:	Justus Peterson-Rhodes
+// Student ID:	jepfn5
+// Section:	A
+// Date: 	09/24/2018
+// File:	hw5.cpp
+// Description:
+//
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  return 0;
+}
