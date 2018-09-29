05301a12c0e8db65fd93b5777d49c1b08be7b8d2
Justus <justus_pete@hotmail.com>
2018-09-29 02:27:59 -0500

    Added function Definitions below main. Improved prototype declarations.

--- hw5.cpp
+++ hw5.cpp
@@ -1,21 +1,49 @@
-// Programmer:	Justus Peterson-Rhodes
-// Student ID:	jepfn5
-// Section:	A
-// Date: 	09/24/2018
-// File:	hw5.cpp
+// Programmer:   Justus Peterson-Rhodes
+// Student ID:   jepfn5
+// Section:      A
+// Date:         09/24/2018
+// File:         hw5.cpp
 // Description:
 //
 //
-
-void greeting();
-float calculateProfit(int x,int y);
-void signoff();
 
 #include <iostream>
 #include <cmath>
 using namespace std;
 
+void greeting();
+float promptValues(float & animalPartsUsed, float & typesOfAnimals);
+float calculateProfit(float animalPartsUsed, float typesOfAnimals);
+void table();
+void signoff();
+
 int main()
 {
+  greeting();
   return 0;
+}
+
+void greeting()
+{
+  cout << "This a greeting to be edited." << endl;
+}
+float promptValues(float animalPartsUsed)
+{
+
+}
+float promptVales(float typesOfAnimals)
+{
+
+}
+float calculateProfit(float & animalPartsUsed, float & typesOfAnimals)
+{
+
+}
+void table()
+{
+
+}
+void signoff()
+{
+  cout << "This is a signoff to be edited." << endl;
 }
